764a086991bec4dc0f2805c97e8529ae6a6cffd9
mostlyCroissant <l.cuello@sivans.jp>
2026-04-16 19:01:06 +0900

    Delete coffee-runner-app directory

--- coffee-runner-app/config.js
+++
@@ -1,7 +0,0 @@
-window.APP_CONFIG = {
-    SHEETS_ENDPOINT: "https://script.google.com/macros/s/AKfycbx7xrVdSRFp1Baca_jKwOjpVZdl2OzTvK7Ma8yYKSJ9BWMPZ5VcLrYs9ei8gjEAv9SH/exec",
-
-  // Local storage keys used on the phone.
-  STORAGE_KEY: "coffee-runner-cart-v1",
-  SELECTED_EVENT_KEY: "coffee-runner-selected-event-v1",
-};
